0f560ef84bf07ba8115ea0c61ad513048c8d41ad
dingchw <503196064@qq.com>
2022-04-07 22:05:24 +0800

    'v202204072205'

--- src/layouts/Menu.vue
+++ src/layouts/Menu.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="menu">
+    <el-row>
+      <el-col :span="1"><div class="grid-content bg-purple-dark" />1234</el-col>
+    </el-row>
     <el-scrollbar>
       <el-menu>
-        <template v-for="(menu,menuIndex) in menuItems" :key="menu.key" :index="menuIndex+''" :value="menu.key">
-        <el-sub-menu v-if="menu.children && menu.children.length > 0" :key="menu.key" :index="menuIndex+''">
-          <template #title>
-            {{getMenuName(menu.key)}}
-          </template>
-          <el-menu-item-group>
-            <el-menu-item v-for="(subMenu, subMenuIndex) in menu.children" 
-            :key="subMenu.key" 
-            :index="menuIndex+''+subMenuIndex+''" 
-            :value="subMenu.key" @click="goPage(subMenu.url)">
-              {{getMenuName(subMenu.key)}}
-            </el-menu-item>
-          </el-menu-item-group>
-        </el-sub-menu>
-        <el-menu-item v-else @click="goPage(menu.url)">
-          {{getMenuName(menu.key)}}
-        </el-menu-item>
+        <template
+          v-for="(menu, menuIndex) in menuItems"
+          :key="menu.key"
+          :index="menuIndex + ''"
+          :value="menu.key"
+        >
+          <el-sub-menu
+            v-if="menu.children && menu.children.length > 0"
+            :key="menu.key"
+            :index="menuIndex + ''"
+          >
+            <template #title>
+              {{ getMenuName(menu.key) }}
+            </template>
+            <el-menu-item-group>
+              <el-menu-item
+                v-for="(subMenu, subMenuIndex) in menu.children"
+                :key="subMenu.key"
+                :index="menuIndex + '' + subMenuIndex + ''"
+                :value="subMenu.key"
+                @click="goPage(subMenu.url)"
+              >
+                {{ getMenuName(subMenu.key) }}
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-menu-item v-else @click="goPage(menu.url)">
+            {{ getMenuName(menu.key) }}
+          </el-menu-item>
         </template>
       </el-menu>
     </el-scrollbar>
@@ -26,38 +41,42 @@
 </template>
 
 <script>
-import menuConfig from '@/layouts/menu.js'
-import locales from '@/locales/index.js'
-export default{
+import menuConfig from "@/layouts/menu.js";
+import locales from "@/locales/index.js";
+export default {
   name: "menu",
-  data(){
+  data() {
     return {
       menuItems: [],
-      localesName: 'zhCN'
-    }
+      localesName: "zhCN",
+    };
   },
-  created(){
+  created() {
     // console.log(locales);
-    menuConfig.map(item=>{
+    menuConfig.map((item) => {
       this.menuItems.push(item);
     });
   },
-  methods:{
-    getMenuName(key){
+  methods: {
+    getMenuName(key) {
       let localesNameMap = locales[this.localesName];
-      let menuName = localesNameMap['MainLayout'][key];
+      let menuName = localesNameMap["MainLayout"][key];
       return menuName;
     },
-    goPage(url){
-      console.log(url)
+    goPage(url) {
+      console.log(url);
       this.$router.push(url);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .el-scrollbar{
-    height: 100%;
-  }
+.menu{
+  height: 100%;
+  border-right: solid 2px black;
+}
+aside,ul{
+  border-right: none;
+}
 </style>
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,35 +1,37 @@
 <template>
-        <el-container>
-            <el-header>
-                <Header/>
-            </el-header>
-            <el-container>
-                <el-aside><Menu/></el-aside>
-                <el-main><Mainlayout/></el-main>
-            </el-container>
-        </el-container>
+  <el-container>
+    <el-header>
+      <Header />
+    </el-header>
+    <el-container>
+      <el-aside><Menu /></el-aside>
+      <el-main><Mainlayout /></el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import {Header,Mainlayout,Menu} from '@/layouts'
-export default{
-    name: "index",
-    components: {
-        Header,
-        Mainlayout,
-        Menu
-    }
-}
+import { Header, Mainlayout, Menu } from "@/layouts";
+export default {
+  name: "index",
+  components: {
+    Header,
+    Mainlayout,
+    Menu,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .el-container{
-        height: 100%;
-    }
-    .el-header{
-        padding: 0;
-    }
-    .el-aside{
-        height: 100%;
-    }
+.el-container {
+  height: 100%;
+}
+.el-header {
+  padding: 0;
+  background-color: rgb(61, 58, 58);
+}
+.el-aside {
+  width: 180px;
+  height: 100%;
+}
 </style>
